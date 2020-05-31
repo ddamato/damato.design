c6dd08b33a48ab1a646a8fe554f903ed63ba291f
Donnie D'Amato <fauxserious@gmail.com>
2020-05-31 01:35:51 -0400

    Fix service worker

--- root/sw.js
+++ root/sw.js
@@ -2,7 +2,6 @@ const cacheName = 'v1';
 
 self.addEventListener('install', () => {
   console.info('Service Worker Installed');
-  return self.skipWaiting();
 });
 
 self.addEventListener('activate', (ev) => {
@@ -15,19 +14,14 @@ self.addEventListener('activate', (ev) => {
   )
 });
 
-self.addEventListener('fetch', (ev) => ev.respondWith(fetchResource(ev.request)));
-
-function fetchResource(request) {
-  return webFetch.catch(async () => await caches.match(request) || new Response());
-}
-
-function webFetch(request) {
-  return fetch(request).then((res) => {
-    if (request.url.startsWith('http')) {
-      const clone = res.clone();
-      caches.open(cacheName).then((cache) => cache.put(request, clone)).catch(() => {});
-    }
-    return res;
-  })
-}
-
+self.addEventListener('fetch', (ev) => {
+  ev.respondWith(
+    fetch(ev.request).then((res) => {
+      if (ev.request.url.startsWith('http')) {
+        const clone = res.clone();
+        caches.open(cacheName).then((cache) => cache.put(ev.request, clone)).catch(() => {});
+      }
+      return res;
+    }).catch(async () => await caches.match(ev.request) || new Response())
+  )
+});
